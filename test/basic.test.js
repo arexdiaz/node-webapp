e2b711ed82a2b7286faf31ae4ce4e9f0576ca754
Alex Sanchez Diaz <arexito@icloud.com>
2025-07-22 13:13:05 -0400

    Update basic.test.js

--- test/basic.test.js
+++ test/basic.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
     it('should return hello message', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello World');
+        expect(res.text).toBe('Hello World II');
     });
 });
