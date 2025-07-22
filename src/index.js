0de0747ef5688e971697eab7371a4dc613042a51
Alex Sanchez Diaz <arexito@icloud.com>
2025-07-22 11:50:05 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(port, () => {
-    console.log('Server running');
-});
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
 
 module.exports = app;
